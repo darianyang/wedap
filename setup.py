0e23f9de0dadd2a56c043ed5b77f812a89c55854
Darian T. Yang <dty7@pitt.edu>
2023-08-28 13:27:45 -0400

    pushing a new pip version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ packages = find_packages()
 
 setup(
     name='wedap',
-    version='0.0.6',
+    version='0.0.7',
     description='weighted ensemble data analysis and plotting',
     long_description=readme,
     long_description_content_type="text/markdown",
