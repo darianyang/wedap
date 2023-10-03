10db61ba6ede3548bdcaecf4c305c2bd13cd54ad
Darian T. Yang <dty7@pitt.edu>
2023-10-03 17:04:53 -0400

    new pip version, bug fix for XY indices, now have tests for them

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ packages = find_packages()
 
 setup(
     name='wedap',
-    version='0.0.9',
+    version='0.0.10',
     description='weighted ensemble data analysis and plotting',
     long_description=readme,
     long_description_content_type="text/markdown",
