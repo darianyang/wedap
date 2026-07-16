04129d1f6fb4fac9611813d8d1dbcafb9411e3e5
Darian Yang <dty7@pitt.edu>
2026-07-16 15:18:14 +0200

    add aliases for easier referencing if needed

--- mdap/__init__.py
+++ mdap/__init__.py
@@ -6,6 +6,12 @@ Description:
 
 """
 
-# Welcome to the ddap module! 
+# Welcome to the mdap module!
 from .md_pdist import *
 from .md_plot import *
+
+# Friendly aliases mirroring wedap.Pdist / wedap.Plot, e.g.
+#     X, Y, Z = mdap.Pdist(Xname="data.dat").pdist()
+#     mdap.Plot(X=X, Y=Y, Z=Z, plot_mode="hist").plot()
+Pdist = MD_Pdist
+Plot = MD_Plot
--- wedap/__init__.py
+++ wedap/__init__.py
@@ -6,7 +6,16 @@ Description:
 
 """
 
-# Welcome to the wedap module! 
+# Welcome to the wedap module!
 from .h5_pdist import *
 from .h5_plot import *
 from .h5_gif import *
+
+# Friendly, array-first aliases. H5_Pdist/H5_Plot are named for their H5 origin,
+# but H5_Plot also accepts plain numpy X/Y/Z arrays directly, e.g.
+#     X, Y, Z = wedap.Pdist("west.h5", "average").pdist()
+#     wedap.Plot(X, Y, Z, plot_mode="hist").plot()
+# Note: numpy arrays should be passed to Plot/H5_Plot (X, Y, Z), NOT to
+# Pdist/H5_Pdist, whose first positional argument is the h5 file path.
+Pdist = H5_Pdist
+Plot = H5_Plot
